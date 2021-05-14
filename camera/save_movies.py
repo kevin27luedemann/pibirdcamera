610525946c25eedfb19646c91171a65b9893a46d
Kevin Luedemann <kevin27luedemann@googlemail.com>
2021-05-14 13:55:09 +0200

    BF

--- camera/save_movies.py
+++ camera/save_movies.py
@@ -68,8 +68,8 @@ def main():
             command += "rm -f {}_before.mp4 && ".format(fname)
             command += "rm -f {}_during.mp4 && ".format(fname)
             command += "rm -f {}_after.mp4 && ".format(fname)
-            command += "rm -f {}_cat.txt".format(fname)
-            print(command)
+            command += "rm -f {}_cat.txt &".format(fname)
+            #print(command)
             os.system(command)
 
     #Stop all recording
